1b9e8c670ed121998e777fa93a90f032ad3e1d43
J.A Venura Jayaasingha <137485589+venura449@users.noreply.github.com>
2023-08-18 07:31:39 +0530

    Add files via upload

---
+++ Lab06/Excersise 03.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+float circleArea(float radius);
+float circlePeri(float radius);
+float recArea(float radius,float height);
+
+int main(){
+    float radius;
+    float height;
+    
+
+}
